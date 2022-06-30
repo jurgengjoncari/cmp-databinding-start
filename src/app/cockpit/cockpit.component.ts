809f4389bb4073f17275af0016bda211b5f41f34
Jurgen Gjonçari <jurgen_gjoncari@icloud.com>
2022-06-30 09:34:01 +0200

    finish viewchild functionality

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output, ViewChild} from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -9,23 +9,24 @@ export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>()
   @Output() blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>()
   
-  @ViewChild('') serverContent
+  @ViewChild('serverContent') serverContent: ElementRef
 
   constructor() { }
 
   ngOnInit(): void {
   }
-  onAddServer(nameInput: HTMLInputElement, contentInput: HTMLInputElement) {
+  onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
       serverName: nameInput.value,
-      serverContent: contentInput.value
+      serverContent: this.serverContent.nativeElement.value
     })
+    
   }
 
-  onAddBlueprint(nameInput: HTMLInputElement, contentInput: HTMLInputElement) {
+  onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
       serverName: nameInput.value,
-      serverContent: contentInput.value
+      serverContent: this.serverContent.nativeElement.value
     })
   }
 }
